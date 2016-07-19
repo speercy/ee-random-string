2164e3d7b8ed79b4bde526fdced9dcdd555d83b5
Steven Peercy <speercy@gmail.com>
2016-07-19 09:39:56 -0400

    Fixing issue with in_array()

--- random_string/addon.setup.php
+++ random_string/addon.setup.php
@@ -5,7 +5,7 @@ return array(
     'author_url'  => 'https://github.com/speercy',
     'name'        => 'Random String',
     'description' => 'Generate random string for ExpressionEngine',
-    'version'     => '2.0',
+    'version'     => '2.1',
     'namespace'   => 'RandomString',
     'docs_url'    => 'https://github.com/speercy/ee-random-string'
 );
--- random_string/pi.random_string.php
+++ random_string/pi.random_string.php
@@ -5,7 +5,7 @@ $plugin_info = array(
     'pi_author_url'  => 'https://github.com/speercy',
     'pi_name'        => 'Random String',
     'pi_description' => 'Generate random string for ExpressionEngine',
-    'pi_version'     => '2.0',
+    'pi_version'     => '2.1',
     'pi_usage'       => 'https://github.com/speercy/ee-random-string'
 );
 
@@ -20,23 +20,19 @@ class Random_string {
         $charset    = explode('|', ee()->TMPL->fetch_param('charset'));
 
         $characters = '';
+        $string     = '';
         $uppers     = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         $lowers     = 'abcdefghijklmnopqrstuvwxyz';
         $digits     = '0123456789';
         $selection  = false;
 
-        if(in_array($charset, 'uppers')) $characters .= $uppers; $selection = true;
-        if(in_array($charset, 'lowers')) $characters .= $lowers; $selection = true;
-        if(in_array($charset, 'digits')) $characters .= $digits; $selection = true;
+        if(in_array('uppers', $charset)) { $characters .= $uppers; $selection = true; }
+        if(in_array('lowers', $charset)) { $characters .= $lowers; $selection = true; }
+        if(in_array('digits', $charset)) { $characters .= $digits; $selection = true; }
 
         if(!$selection) $characters .= $uppers.$lowers.$digits;
 
-        $string     = '';
-
-        for ($p = 0; $p < $length; $p++)
-        {
-            $string .= $characters[mt_rand(0, (strlen($characters)-1))];
-        }
+        for ($p = 0; $p < $length; $p++) { $string .= $characters[mt_rand(0, (strlen($characters)-1))]; }
 
         $this->return_data = $string;
     }
